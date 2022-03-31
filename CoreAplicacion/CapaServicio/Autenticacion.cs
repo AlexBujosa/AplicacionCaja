f409cea77f0e9dcdf33a319738d7abc46cbd3568
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-03-31 18:29:49 -0400

    Agregar metodo Autenticar y TodosDatoCliente

--- CoreAplicacion/CapaServicio/Autenticacion.cs
+++ CoreAplicacion/CapaServicio/Autenticacion.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
-
+using System.Data.SqlClient;
 namespace CoreAplicacion.CapaServicio
 {
     public class Autenticacion
     {
+        public string ConnectionStrings;
+        SqlConnection connection;
+        SqlCommand sqlCommand;
+        public DataSet Autenticarse(string Usuario, string Contraseña, int Pin)
+        {
+            DataSet Auth = new DataSet();
+            Controlador controlador = new Controlador();
+            ConnectionStrings = controlador.ObtenerConexion();
+            connection.ConnectionString = ConnectionStrings;
+            connection.Open();
+            SqlDataReader reader = Autenticar(Usuario, Contraseña, Pin);
+            int ID_Cliente;
+
+            if (reader.Read())
+            {
+                ID_Cliente = int.Parse(reader["ID_Cliente"].ToString());
+            }
+
+            return Auth;
+        }
+        public DataSet TodosDatoCliente(int ID_Cliente)
+        {
+            DataSet Auth = new DataSet();
+            sqlCommand = new SqlCommand();
+            return Auth;
+        }
+        public SqlDataReader Autenticar(string Usuario, string Contraseña, int Pin)
+        {
+            sqlCommand = new SqlCommand();
+            sqlCommand.CommandText = "ppGetAutenticarse";
+            sqlCommand.Parameters.AddWithValue("@Usuario", Usuario);
+            sqlCommand.Parameters.AddWithValue("@Contra", Contraseña) ;
+            sqlCommand.Parameters.AddWithValue("@Pin", Pin);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCommand.Connection = connection;
+            SqlDataReader datareader = sqlCommand.ExecuteReader();
+            return datareader;
+        }
     }
 }
---
+++ CoreAplicacion/Controlador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+namespace CoreAplicacion
+{
+    
+    public class Controlador
+    {
+        public string ObtenerConexion()
+        {
+            return ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        }
+    }
+}
