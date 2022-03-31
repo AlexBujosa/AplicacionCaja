3ec7889b6c38eb681dada98ba4235081355a251a
zolanovich <avilajuan0301@gmail.com>
2022-03-31 00:57:08 -0400

    Modelo inicial integracion

---
+++ Integracion/IntegracionASMX.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+namespace Integracion
+{
+    /// <summary>
+    /// Descripción breve de IntegracionASMX
+    /// </summary>
+    [WebService(Namespace = "http://integracionFigureBank.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class IntegracionASMX : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataTable Autenticacion(//args)
+        {
+
+            DataTable auth = new DataTable();
+            auth = Autenticacion(//args) //autenticacion del core, que ejecutará el select
+            return auth;
+        }
+    }
+}
